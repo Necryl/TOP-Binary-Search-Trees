327104e6a0f96d730e1307145ba8ed2354ee599c
Necryl <necrylthewayfarer@gmail.com>
2023-01-18 12:58:13 +0530

    create factory functions: Node, Tree, and supporting function buildTree and copy-paste a prettyPrint function to display the tree structure visually in teh console

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,40 @@
+import _ from "lodash";
 import mainStyles from "./../styles/main.css";
+
+function Node(data = null, left = null, right = null) {
+  return {
+    data,
+    left,
+    right,
+  };
+}
+
+function buildTree(array, start = 0, end = array.length - 1) {
+  if (start > end) {
+    return null;
+  }
+  let mid = Math.round(start + (end - start) / 2);
+  let leftSubtree = buildTree(array, start, mid - 1);
+  let rightSubtree = buildTree(array, mid + 1, end);
+
+  return Node(array[mid], leftSubtree, rightSubtree);
+}
+
+function Tree(array) {
+  array = _.sortBy(_.uniq(array));
+  let root = buildTree(array);
+  return { root };
+}
+
+// prettyPrint function is copied from TOP lesson
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(Tree([1, 2, 3]).root);
